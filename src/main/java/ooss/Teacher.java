50f2865db89618c69a33de320ceae62b1b68b819
munoztr <munoztr@oocl.com>
2022-08-31 17:28:07 +0800

    Test: check if all tests in step 2 and 3 are passed

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -3,6 +3,8 @@ package ooss;
 import java.text.MessageFormat;
 
 public class Teacher extends Person{
+
+
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
@@ -18,4 +20,5 @@ public class Teacher extends Person{
     public void assignTo(Klass klass) {
         this.setKlass(klass);
     }
+
 }
--- src/test/java/ooss/step2/TeacherTest.java
+++ src/test/java/ooss/step2/TeacherTest.java
@@ -23,20 +23,20 @@ public class TeacherTest {
 
         assertEquals("My name is Robert. I am 20 years old. I am a teacher.", introduce);
     }
-//
-//    @Test
-//    public void should_return_true_when_equals_given_two_teachers_with_same_id() {
-//        Teacher teacher1 = new Teacher(1, "Jerry", 21);
-//        Teacher teacher2 = new Teacher(1, "Jerry", 22);
-//
-//        assertEquals(teacher1, teacher2);
-//    }
-//
-//    @Test
-//    public void should_return_false_when_equals_given_two_teachers_with_different_id() {
-//        Teacher teacher1 = new Teacher(1, "Jerry", 21);
-//        Teacher teacher2 = new Teacher(2, "Jerry", 21);
-//
-//        assertNotEquals(teacher1, teacher2);
-//    }
+
+    @Test
+    public void should_return_true_when_equals_given_two_teachers_with_same_id() {
+        Teacher teacher1 = new Teacher(1, "Jerry", 21);
+        Teacher teacher2 = new Teacher(1, "Jerry", 22);
+
+        assertEquals(teacher1, teacher2);
+    }
+
+    @Test
+    public void should_return_false_when_equals_given_two_teachers_with_different_id() {
+        Teacher teacher1 = new Teacher(1, "Jerry", 21);
+        Teacher teacher2 = new Teacher(2, "Jerry", 21);
+
+        assertNotEquals(teacher1, teacher2);
+    }
 }
