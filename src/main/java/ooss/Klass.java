caebe8976ffdbdbef3556eda71e3f301c93e2289
munoztr <munoztr@oocl.com>
2022-08-31 18:38:11 +0800

    feat: check if student is in class when checking if he is leader

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -4,6 +4,15 @@ import java.util.Objects;
 
 public class Klass {
     private final int id;
+    private Student classLeader;
+
+    public Student getClassLeader() {
+        return classLeader;
+    }
+
+    public void setClassLeader(Student classLeader) {
+        this.classLeader = classLeader;
+    }
 
     public Klass(int id) {
         this.id = id;
@@ -26,4 +35,10 @@ public class Klass {
         return Objects.hash(id);
     }
 
+    public boolean isLeader(Student tom) {
+        if (tom.getKlass() != null) {
+            return (tom.getKlass().equals(this));
+        }
+        else  return false;
+    }
 }
