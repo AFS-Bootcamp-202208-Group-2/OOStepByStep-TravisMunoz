2b03926a8540f2aebf414d8ca9e414ac9b62d307
munoztr <munoztr@oocl.com>
2022-08-31 20:22:27 +0800

    feat: student will say stuff when class leader is assigned.

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -1,13 +1,27 @@
 package ooss;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Klass {
     private final int id;
     private Student classLeader;
 
     private Teacher teacher;
+
+    public Student getStudentWatch() {
+        return studentWatch;
+    }
+
+    public void setStudentWatch(Student studentWatch) {
+        this.studentWatch = studentWatch;
+    }
+
+    private Student studentWatch;
+
 
     public Teacher getTeacher() {
         return teacher;
@@ -56,15 +70,25 @@ public class Klass {
     }
 
     public void assignLeader(Student tom) {
-        if(tom.getKlass()==this) {
+        if (tom.getKlass() == this) {
             this.setClassLeader(tom);
-            String message = MessageFormat.format("I am {0}, teacher of Class {1}. I know {2} become Leader.",teacher.getName(),id,tom.getName());
-            System.out.println(message);
-        }
-        else System.out.println("It is not one of us.");
+            if (teacher != null) {
+                String message = MessageFormat.format("I am {0}, teacher of Class {1}. I know {2} become Leader.", teacher.getName(), id, tom.getName());
+                System.out.println(message);
+            }
+            if (studentWatch != null) {
+                String message = MessageFormat.format("I am {0}, student of Class {1}. I know {2} become Leader.", studentWatch.getName(), id, tom.getName());
+                System.out.println(message);
+            }
+
+        } else System.out.println("It is not one of us.");
     }
 
     public void attach(Teacher jerry) {
         setTeacher(jerry);
+    }
+
+    public void attach(Student student) {
+        setStudentWatch(student);
     }
 }
