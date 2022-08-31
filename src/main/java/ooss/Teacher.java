860a37f26899163044eaaa26942c9d85ee90ec6a
munoztr <munoztr@oocl.com>
2022-08-31 18:23:34 +0800

    feat: added method to display class list of teacher

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -2,7 +2,11 @@ package ooss;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 public class Teacher extends Person{
 
@@ -20,7 +24,20 @@ public class Teacher extends Person{
     }
 
     public String introduce() {
-        return MessageFormat.format("My name is {0}. I am {1} years old. I am a teacher." ,super.getName(),super.getAge());
+        String message =  MessageFormat.format("My name is {0}. I am {1} years old. I am a teacher." ,super.getName(),super.getAge());
+        List<Integer> klasses = new ArrayList<>();
+        String klassesDelimited;
+        if (this.getKlassList() !=null)
+        {
+            message += " I teach Class ";
+//            klasses=  this.getKlassList().stream().map(Klass::getId).collect(Collectors.joining(" and ")
+            klasses = this.getKlassList().stream().map(Klass::getId).collect(toList());
+            klassesDelimited = klasses.stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(", "));
+            message += klassesDelimited +".";
+        }
+        return message;
     }
 
     public boolean belongsTo(Klass klass) {
